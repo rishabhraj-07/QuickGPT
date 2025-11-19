91b38107bdd9ac65afd08f11baa4bd2ba1eb45e1
Rishabh Raj <rajrishabh077@gmail.com>
2025-11-19 20:23:42 +0530

    Add inital files

--- Frontend/utils/SecretToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import "dotenv/config";
-
-export const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: 3 * 24 * 60 * 60,
-  });
-};
